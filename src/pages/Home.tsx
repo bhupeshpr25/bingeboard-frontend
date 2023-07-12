09591181fe00666e6d9cd41997bc74cf082dfc4e
Bhupesh <pradhanbhupesh0@gmail.com>
2023-07-12 23:16:23 +0530

    fix: remove unused code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import { getMedia } from "../services/apiMedia";
 import MediaListContainer from "../components/Media/MediaListContainer";
-import { useToast } from "@chakra-ui/react";
-import { useEffect } from "react";
 
 interface MediaItem {
   id: string;
@@ -14,34 +11,13 @@ interface MediaItem {
 }
 
 const Home = () => {
-  const { user: authUser, getToken } = useAuth();
-  const navigate = useNavigate();
-  const toast = useToast();
+  const { getToken } = useAuth();
 
   const token = getToken() ?? "";
 
   const { isLoading, isError } = useQuery<MediaItem[], Error>(["media"], () =>
     getMedia(token)
   );
-
-  // if (!authUser) {
-  //   toast({
-  //     title: "Account not found",
-  //     description: "Redirecting to sign in page",
-  //     status: "warning",
-  //     duration: 5000,
-  //     isClosable: true,
-  //   });
-
-  //   navigate("/signin");
-  // }
-
-  // useEffect(() => {
-  //   if (!authUser) {
-  //     console.log("no account");
-  //     navigate("/signin");
-  //   }
-  // }, [authUser, navigate]);
 
   if (isLoading) {
     return <div>Loading your media...</div>;
